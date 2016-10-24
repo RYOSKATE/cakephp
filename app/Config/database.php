872d370485af7dc271b5ed0fe69b0f1ac9a91705
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2016-10-24 15:20:59 +0900

    fix database setting

--- app/Config/database.php
+++ app/Config/database.php
@@ -69,7 +69,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
+		'password' => 'crmsn213',
 		'database' => 'visualizetool',
 		'prefix' => '',
 		//'encoding' => 'utf8',
